04179616bca201def3f039d6441592d26ea19b74
Jeeva <108859699+Jeevs18@users.noreply.github.com>
2022-07-22 05:19:28 +0530

    Add files via upload

---
+++ day3task2.js
@@ -0,0 +1,63 @@
+   {
+     "basics" [
+        {
+      "name": "Jeeva subramaniyam",
+       "email": "sjeeva2105@gmail.com",
+      "phone": "9123456789",
+      "degree": "MBA",
+        }
+    ]
+    
+
+    "work" [
+      {
+        "company": "Huron consulting services",
+        "position": "Senior process analyst",
+        "startDate": "2022-03-24",
+        "endDate": "",
+       }
+    ],
+
+    "education" [
+      {
+        "institution": "Sasureie college of arts and science",
+        "area": "Commerce with computer application",
+        "grade": "Distinction",
+ 
+      }
+    ],
+
+    "awards" [
+      {
+        "title": "Employee of the month",
+        "date": "2022-07-07",
+        "awarder": "Chief executive director",
+        }
+    ],
+
+    "skills" [
+      {
+        "name": "Full stack developer",
+        "level": "beginner",
+        "keywords": [
+          "'Javascript', 'C++' and Html)"
+        ]
+      }
+    ],
+
+    "languages" [
+      {
+        "language": "Tamil and English",
+        "fluency": "Your language fluency"
+      }
+    ],
+
+    "interests" [
+      {
+        "name": "Sports",
+        "keywords": [
+          "Cricket' , 'Football"
+        ]
+      }
+    ]
+}
